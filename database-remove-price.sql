1f0a696c8707bb5a2999823234bb035ac0ddfd41
Jezzn <jet@in2it.co.th>
2026-01-21 16:10:28 +0700

    clear file

--- database-remove-price.sql
+++
@@ -1,8 +0,0 @@
--- Remove price-related columns since tickets are free
--- Run this on existing database
-
--- Remove total_amount from bookings table
-ALTER TABLE bookings DROP COLUMN IF EXISTS total_amount;
-
--- Remove price from tiers table (optional - keep for reference but not used)
--- ALTER TABLE tiers DROP COLUMN IF EXISTS price;
